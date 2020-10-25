8b79b1c54d7d2fb4b4d96d27788997fb1e71a1c6
Al-Aswad <73427522+Al-Aswad@users.noreply.github.com>
2020-10-26 06:44:58 +0800

    Add files via upload

--- assets/javascript.js
+++ assets/javascript.js
@@ -5,7 +5,38 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass('sticky')
         }
+        if (this.scrollY > 500 ){
+            $('.scroll-up-btn').addClass('show')
+        }else{
+            $('.scroll-up-btn').removeClass('show')
+        }
     });
+
+    //slide up
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0
+        });
+    });
+
+    //typing animation
+
+    var typed = new Typed(".typing",{
+        strings :["Design Web","Wordpress","Web Aplication"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    var typed = new Typed(".typing-1",{
+        strings :["Design Web","Wordpress","Web Aplication"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    
+
+
     // toggle menu
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
